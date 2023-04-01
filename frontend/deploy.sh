13eeaafbd3dcb28943c786cbdaa3703d687c4070
Frank Nguyen <phucnguyen.snow@gmail.com>
2023-04-01 19:15:10 +0700

    Update deploy script

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,3 +1,3 @@
 rm -rf /var/www/mealhelper-docker/frontend/src
 mkdir -p /var/www/mealhelper-docker/frontend/src
-cp /var/www/mealhelper-fe/* /var/www/mealhelper-docker/frontend/src
+cp -r /var/www/mealhelper-fe/* /var/www/mealhelper-docker/frontend/src/
